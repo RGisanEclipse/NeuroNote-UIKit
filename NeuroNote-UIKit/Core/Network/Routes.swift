d0da4de7038633d0f1d33e0e5728d194cb745b93
Rishab <rishab28guleria@gmail.com>
2025-09-10 21:59:38 +0530

    Moved OTP API urls to OTP service

--- NeuroNote-UIKit/Core/Network/Routes.swift
+++ NeuroNote-UIKit/Core/Network/Routes.swift
@@ -12,7 +12,7 @@ enum Routes {
     static let base = "https://127.0.0.1:8443"   // Base URL, shall be updated to production later
     static let signUp = "/api/v1/auth/signup"
     static let signIn = "/api/v1/auth/signin"
-    static let requestOTP = "/api/v1/auth/otp/request"
-    static let verifyOTP = "/api/v1/auth/otp/verify"
+    static let requestOTP = "/api/v1/otp/request"
+    static let verifyOTP = "/api/v1/otp/verify"
     static let refreshToken = "/api/v1/auth/token/refresh"
 }
