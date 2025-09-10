e4fc42bf7126f8f21868394d6efce773bc857461
Rishab <rishab28guleria@gmail.com>
2025-09-10 21:55:13 +0530

    Updated API URLs

--- NeuroNote-UIKit/Core/Network/Routes.swift
+++ NeuroNote-UIKit/Core/Network/Routes.swift
@@ -10,9 +10,9 @@ import Foundation
 enum Routes {
 
     static let base = "https://127.0.0.1:8443"   // Base URL, shall be updated to production later
-    static let signUp = "/signup"
-    static let signIn = "/signin"
-    static let requestOTP = "/auth/otp/request"
-    static let verifyOTP = "/auth/otp/verify"
-    static let refreshToken = "/auth/refreshtoken"
+    static let signUp = "/api/v1/auth/signup"
+    static let signIn = "/api/v1/auth/signin"
+    static let requestOTP = "/api/v1/auth/otp/request"
+    static let verifyOTP = "/api/v1/auth/otp/verify"
+    static let refreshToken = "/api/v1/auth/token/refresh"
 }
